537bd7e9a282ae5fc8b7f70e4947778983b93d0d
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2023-02-15 00:24:51 +0300

    Update CopterGurru.py

--- CopterGurru.py
+++ CopterGurru.py
@@ -1,13 +1,18 @@
 import telebot
 import random as r
+from telebot import types
 import time
+import os, subprocess
 #-- Мои библиотеки--
 from detals import cpicok
 
-bot = telebot.TeleBot('---')
+bot = telebot.TeleBot('6212415386:AAGTkDPFgrFlBToaysUdvOEMBIuQj24T8AA')
 print("Бот запущен!")
 file = open('detals.txt','r')
-@bot.message_handler(commands=['help','secret','list'])
+bank_file = open('bank.txt','r')
+CopterCoin = 0
+kvest = 0
+@bot.message_handler(commands=['help','secret','list','/lider_board, /balance'])
 def comands(message):
      if message.text == '/help':
          bot.send_message(message.chat.id, '/secret - секрет \n /list - список всех деталей')
@@ -21,4 +26,37 @@ def comands(message):
         with open('detals.txt') as file:
             while (line := file.readline().rstrip()):
                 bot.send_message(message.chat.id, line)
+
+     if message.text == '/lider_board':
+        with open('ban.txt') as file:
+            while (line := file.readline().rstrip()):
+                bot.send_message(message.chat.id, line)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    global kvest,CopterCoin
+    if (message.text == "Подсчет деталей") and (kvest == 0):
+        bot.send_message(message.from_user.id, 'Хорошо пиши название детали и их количество \n Например "Ключ для пропеллеров 5" Напиши Стоп, когда закончишь подсчет')
+        CopterCoin += 10
+        kvest = 1
+
+    elif (message.text == "Стоп") and (kvest == 1):
+        bot.send_message(message.from_user.id, "Молодец! Твои труды не будут безвозмездны. Вот тебе " + str(CopterCoin) + " коптеркоинов")
+        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEHuRxj6jZgWzfsg8kXz2uPYU5JXD3hvwACOBkAAntfQEmO99QKq9iyTC4E')
+        cpicok.writeBank(message.from_user.first_name,CopterCoin)
+        kvest = 0
+
+    elif kvest == 1:
+        bot.send_message(message.from_user.id, cpicok.search(message.text))
+        btn1 = types.KeyboardButton("👋 Поздороваться")
+        btn2 = types.KeyboardButton("❓ Задать вопрос")
+
+    elif message.text == "Регистрация":
+        name = str(message.from_user.first_name)
+        bot.reply_to(message, "Привет!" + name + "\n Добро пожаловать в нашу команду)")
+        bot.send_message(message.from_user.id, "Напиши привет")
+
+    elif message.text == "Дай квест":
+        bot.send_message(message.from_user.id, "Напиши привет")
+
 bot.polling()
